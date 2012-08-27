908ba24e9d749b42eefb31406078e6301cfc3ba0
Franco Reyes <freyes1988@gmail.com>
2012-08-26 21:34:29 -0500

    Removed modules so that I can just create extensions based off lotto

--- lib/models/mega_millions.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from datetime import date
-
-class mega_millions:
-    def __init__(self,game,numbers,mega_number,draw_date):
-        self.game= game
-        self.nums = numbers
-        self.draw_date = draw_date
