cc09d9eda9e6c58bd8870646b4492fdb4d471d69
Franco Reyes <freyes1988@gmail.com>
2012-12-30 19:26:18 -0600

    Test script now prints out results by going through the list of lotto objects and printing by calling the accessors of the lotto objects

--- test.py
+++ test.py
@@ -5,25 +5,21 @@ import sys
 import lib.models.lotto
 import lib.factories.lotto
 
-numbers = [3,2,4]
-
-lotto_obj = lib.models.lotto.lotto(3,numbers,'20120623')
-
-print lotto_obj.game
-print lotto_obj.numbers
-
 print "-"*5
 print "Test Factories"
 print "-"*5
 
 lotto_fact = lib.factories.lotto.lotto_factory('Lotto');
-lot_json = lotto_fact.retrieve_lotto('Lotto');
-
-#for lot in lot_json:
-#    lot.print
+#lot_json = lotto_fact.retrieve_lotto('Lotto');
+lot_ar = lotto_fact.retrieve_lotto();
 
 list_format = "{!s:15}{!s:10}{!s:10}"
 print list_format.format('Date','Game','Numbers')
 
-for lot in lot_json:
-    print list_format.format(lot['col1'],lot['col2'],lot['col3'])
+counter = 0
+for lot in lot_ar:
+    if counter % 25 == 0:
+        print list_format.format('Date','Game','Numbers')
+        print '-'*35
+    print list_format.format(lot.get_draw_date(),lot.get_game()," ".join(lot.get_numbers()))
+    counter +=1
