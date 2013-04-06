3b185ab8a0f06b37e2276f69ad9262595800c1f0
Franco Reyes <freyes1988@gmail.com>
2013-04-06 12:07:29 -0500

    Fixed syntax errors in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bion/env python
+#!/usr/bin/env python
 
 from distutils.core import setup
 
@@ -6,6 +6,6 @@ setup(name='Lotto_analysis',
     version='0.1',
     description='Simple lotto analysis project',
     author='Franco Reyes',
-    author_email='freyes1988@gmail.com'
+    author_email='freyes1988@gmail.com',
     packages=['lotto_analysis'],
 )
