c17fb35d66f1f71eb6a86d4c96fb7b0e2c20f2f0
Franco Reyes <freyes1988@gmail.com>
2013-04-06 11:15:25 -0500

    Wrote simple setup.py based off the example provided in docs.python.org

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bion/env python
+
+from distutils.core import setup
+
+setup(name='Lotto_analysis',
+    version='0.1',
+    description='Simple lotto analysis project',
+    author='Franco Reyes',
+    author_email='freyes1988@gmail.com'
+    packages=['lotto_analysis'],
+)
